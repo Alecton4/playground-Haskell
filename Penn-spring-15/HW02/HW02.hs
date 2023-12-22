cd3a52f45eb930e34f07d3631d810db3010c8e4a
az-P1-openSUSE <yzhaocl@connect.ust.hk>
2023-12-22 18:10:10 +0800

    feat(Penn): finish exercise `[1..5]` of HW02

--- Penn-spring-15/HW02/HW02.hs
+++ Penn-spring-15/HW02/HW02.hs
@@ -24,38 +24,58 @@ colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
 -- Get the number of exact matches between the actual code and the guess
 exactMatches :: Code -> Code -> Int
-exactMatches = undefined
+-- exactMatches xs ys = sum [if x == y then 1 else 0 | x <- xs, y <- ys]
+-- exactMatches xs ys = sum [i | x <- xs, y <- ys, let i = if x == y then 1 else 0]
+-- NOTE: The above two implementations are wrong.
+exactMatches xs ys = sum (zipWith (\x y -> if x == y then 1 else 0) xs ys)
 
 -- Exercise 2 -----------------------------------------
 
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
-countColors = undefined
+-- From Phind:
+-- > `map` applies the lambda function to each `Peg` in `colors`.
+-- > The lambda function uses `filter` to create a list of elements in `code`
+-- > that are equal to `color`,
+-- > and `length` to count the number of elements in this list.
+-- > The result is a list of counts of each `Peg` in `code`,
+-- > in the same order as `colors`.
+countColors code = map (\color -> length (filter (== color) code)) colors
+
+-- `countColors` can be separated into two functions:
+-- countColors :: Code -> [Int]
+-- countColors code = map (countSingleColor code) colors
+-- countSingleColor :: Code -> Peg -> Int
+-- countSingleColor code color = length (filter (== color) code)
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches xs ys = sum (zipWith min (countColors xs) (countColors ys))
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove secret guess = Move guess exact nonExact
+  where
+    exact = exactMatches secret guess
+    nonExact = matches secret guess - exact
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent (Move guess exact nonExact) code =
+  (exact == exactMatches guess code) && (nonExact + exact == matches guess code)
 
 -- Exercise 5 -----------------------------------------
 
 filterCodes :: Move -> [Code] -> [Code]
-filterCodes = undefined
+filterCodes move = filter $ isConsistent move
 
 -- Exercise 6 -----------------------------------------
 
 allCodes :: Int -> [Code]
-allCodes = undefined
+allCodes n = undefined
 
 -- Exercise 7 -----------------------------------------
 
